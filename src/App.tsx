c47118fd7fd552cb173eb7adb71a61d68c854527
Marcin Gąsowski <43890457+MGasowski@users.noreply.github.com>
2022-06-06 12:54:56 +0200

    Update App.tsx
    removed unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import AirtableComp from "./components/Airtable";
 import Layout from "./Layout";
 import routes from "./routes";
 
